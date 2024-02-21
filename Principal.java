ad86e502a3fd08f18ef393c8f1cf5309cd85f817
yagomonteiroo <yago.monteiroo@gmail.com>
2024-02-21 18:55:25 -0300

    indent

--- Principal.java
+++ Principal.java
@@ -1,18 +1,18 @@
 public class Principal {
-public static void main(String[] args) {
-  Funcionario joao = new Funcionario();
-  joao.setNome("João");
-  joao.setSalario(1000);
+  public static void main(String[] args) {
+    Funcionario joao = new Funcionario();
+    joao.setNome("João");
+    joao.setSalario(1000);
 
-  System.out.println(joao.getNome() + " recebe um salário de R$" + joao.getSalario());
+    System.out.println(joao.getNome() + " recebe um salário de R$" + joao.getSalario());
 
 
-  Coordenador coordenadorCursoJava = new Coordenador();
-  coordenadorCursoJava.setQuantidadeAlunos(8);
+    Coordenador coordenadorCursoJava = new Coordenador();
+    coordenadorCursoJava.setQuantidadeAlunos(8);
 
-  joao = coordenadorCursoJava;
+    joao = coordenadorCursoJava;
 
-  System.out.println(joao.getSalario());
+    System.out.println(joao.getSalario());
 
-}
+  }
 }
